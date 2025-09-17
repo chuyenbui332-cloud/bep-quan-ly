afaaac9a4c7f3941ea927f827cadec828630e12d
chuyenbui332-cloud <chuyenbui332@gmail.com>
2025-09-17 10:25:13 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,26 +1,26 @@
 // db.js
+require("dotenv").config(); // để đọc file .env
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",   // đổi theo DB của bạn
-  user: "root",
-  password: "Hoilamgi2807@",  // nhớ bảo mật sau này
-  database: "quanlybep",
-  port: 3306,   // ⚡ thêm dòng này
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
-// Kiểm tra DB hiện tại ngay khi server start
+
+// Kiểm tra DB khi server start
 (async () => {
   try {
     const [rows] = await pool.query("SELECT DATABASE() AS db");
-    console.log("👉 Đang kết nối database:", rows[0].db);
+    console.log("👉 Kết nối thành công DB:", rows[0].db);
   } catch (err) {
     console.error("❌ Lỗi kết nối DB:", err.message);
   }
 })();
-
-
 
 module.exports = pool;
