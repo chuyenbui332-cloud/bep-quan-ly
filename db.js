bface04ed40d5e356f2b1803d6bf26986767a60a
chuyenbui332-cloud <chuyenbui332@gmail.com>
2025-09-17 16:20:19 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,9 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+import mysql from "mysql2/promise";
 
-const connection = mysql.createConnection(process.env.MYSQL_URL);
+const connection = await mysql.createConnection(process.env.MYSQL_URL);
 
-connection.connect(err => {
-  if (err) {
-    console.error('❌ Lỗi kết nối DB:', err);
-    process.exit(1);
-  } else {
-    console.log('✅ Đã kết nối DB thành công!');
-  }
-});
+export default connection;
+
 
 module.exports = connection;
